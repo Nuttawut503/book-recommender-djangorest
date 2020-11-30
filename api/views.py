8f84a48e4a9c10a2d47b8f8cc7c5b302ec6e2744
Nuttawut503 <einsxort127@gmail.com>
2020-11-30 08:03:28 +0000

    cleaning-files: delete comments

--- api/views.py
+++ api/views.py
@@ -55,10 +55,6 @@ def predict_books(request):
         candidates = candidates.append(similarities)
     candidates = candidates.groupby(candidates.index).sum()
     candidates.sort_values(inplace = True, ascending = False)
-<<<<<<< HEAD
-=======
-    candidates.index += 1
->>>>>>> 05e72eda954d143b9d9fc11dd1583e1c3257fa2f
     candidates = candidates.drop(my_rating.index)[:10]
     results = books_csv[books_csv.id.isin(candidates.index)]
     return Response(to_json(results))
@@ -68,71 +64,3 @@ def list_of_books(request):
     id_list = json.loads(request.data.get('id_list'))
     results = books_csv[books_csv.id.isin([*map(int, id_list)])]
     return Response(to_json(results))
-
-# from django.shortcuts import render
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# import json
-# import pandas as pd
-# books_csv = pd.read_csv('/root/djangorest/api/books_full.csv')
-# books_csv = books_csv[books_csv['id'] < 3001]
-# ratings_csv = pd.read_csv('/root/djangorest/api/ratings_full.csv')
-# ratings_csv = ratings_csv[ratings_csv['book_id'] < 3001]
-# corrs_csv = ratings_csv.pivot_table(index=['user_id'], columns=['book_id'], values='rating').corr()#pd.read_csv('/root/djangorest/api/corrs_cut.csv')
-
-# def to_json(filtered_books, ratings_included=False):
-#     json = []
-#     for i in filtered_books.index:
-#         book = {
-#             'id': str(filtered_books.id[i]),
-#             'title': str(filtered_books.title[i]),
-#             'author': str(filtered_books.authors[i]),
-#             'img_url': str(filtered_books.image_url[i]),
-#             'avg_score': float(filtered_books.average_rating[i]),
-#         }
-#         if ratings_included:
-#             book['counting_scores'] = [
-#                 int(filtered_books['ratings_1'][i]),
-#                 int(filtered_books['ratings_2'][i]),
-#                 int(filtered_books['ratings_3'][i]),
-#                 int(filtered_books['ratings_4'][i]),
-#                 int(filtered_books['ratings_5'][i]),
-#             ]
-#         json.append(book)
-#     return json
-
-# # Create your views here.
-
-# @api_view(['POST'])
-# def search_books(request):
-#     query = str(request.data.get('query'))
-#     columns = ['title', 'authors']
-#     results = books_csv[books_csv[columns].apply(lambda column: column.str.contains(query, case=False)).any(axis=1)]
-#     return Response(to_json(results))
-
-# @api_view(['POST'])
-# def more_book_info(request):
-#     book_id = int(request.data.get('book_id'))
-#     results = books_csv[books_csv.id == book_id]
-#     return Response(to_json(results, ratings_included=True))
-
-# @api_view(['POST'])
-# def predict_books(request):
-#     rating_book = json.loads(request.data.get('rating_books'))
-#     my_rating = pd.Series(map(float, rating_book.values()), index=map(int, rating_book.keys()))
-#     candidates = pd.Series()
-#     for i in my_rating.index:
-#         similarities = corrs_csv[i].dropna()
-#         similarities = similarities.map(lambda x: x * my_rating[i])
-#         candidates = candidates.append(similarities)
-#     candidates = candidates.groupby(candidates.index).sum()
-#     candidates.sort_values(inplace = True, ascending = False)
-#     candidates = candidates.drop(my_rating.index)[:16]
-#     results = books_csv[books_csv.id.isin(candidates.index)]
-#     return Response(to_json(results))
-
-# @api_view(['POST'])
-# def list_of_books(request):
-#     id_list = json.loads(request.data.get('id_list'))
-#     results = books_csv[books_csv.id.isin([*map(int, id_list)])]
-#     return Response(to_json(results))
